08dd8a38839420619bda89b35b213225107a63d4
zubairabid <zubairabid1999+github@gmail.com>
2018-03-21 01:43:03 +0530

    Up to date

--- Ass/3/basic_curses.py
+++ Ass/3/basic_curses.py
@@ -5,16 +5,23 @@ import time
 def main(stdscr):
     # Make stdscr.getch non-blocking
     stdscr.nodelay(True)
+    curses.curs_set(0)
     stdscr.clear()
 
+    stdscr.resize(24, 80)
     while True:
         c = stdscr.getch()
+        if(c == -1):
+            c = ord('A');
         # Clear out anything else the user has typed in
         curses.flushinp()
-        stdscr.clear()
+        stdscr.clear() # also repaints
 
+        # stdscr.border([0[, 0[, 0[, 0[, 0[, 0[, 0[, 0]]]]]]]])
+        # stdscr.border('|','|','-','-','/','\\','\\','/')
+        stdscr.border(0, 0, 0, 0, 0, 0, 0, 0)
 
-
+        stdscr.resize(24, 80)
         # Wait 1/10 of a second. Read below to learn about how to avoid
         # problems with using time.sleep with getch!
         time.sleep(0.1)
